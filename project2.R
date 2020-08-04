b0180120315e86b28d8a6d0212bb268e650ddd15
Joon <mambady0325@gmail.com>
2020-08-04 17:46:24 -0400

    make total data

--- project2.R
+++ project2.R
@@ -10,3 +10,28 @@ data_R<-read.table("wineQualityReds.csv", header=TRUE, sep=",")[,-1]
 table(is.na(data_W))
 table(is.na(data_R))
 
+# add which wine column
+wine <- rownames(data_W)
+wine[1:length(wine)] <- "w"
+
+data_W <- as.data.frame(cbind(data_W, wine))
+
+wine <- rownames(data_R)
+wine[1:length(wine)] <- "r"
+
+data_R <- as.data.frame(cbind(data_R, wine))
+
+# make one data frame with all
+data_t <- as.data.frame(rbind(data_W, data_R))
+
+# save the total data
+write.table(data_t, "data_t.txt", col.names=T, row.names=F, sep="\t", quote=F)
+
+################################
+##################################
+##################################
+# read in the data  
+data <- read.table("data_t.txt", header=T, sep="\t")
+
+
+
